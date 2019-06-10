9613fe7ea98d1bcde229c3a92939ca4d32804d0c
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-06-10 14:38:56 +0800

    add basic search text input

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,64 @@ import React from "react";
 import logo from "../assets/logo.svg";
 import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.foods = [
+      { id: 1, type: "apple", calorie: 100 },
+      { id: 2, type: "banana", calorie: 50 }
+    ];
+    this.state = {
+      text: null
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ text: document.body.querySelector("#food-search").value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Calorie Tracker</h1>
+        <input
+          id="food-search"
+          type="text"
+          placeholder="Search for food item here"
+        />
+        <button onClick={this.handleClick}>Search</button>
+        <h2>Search results for {this.state.text ? this.state.text : "..."}</h2>
+        <SearchResults list={this.foods} searchInput={this.state.text} />
+      </div>
+    );
+  }
+}
+
+function SearchResults(props) {
+  let searchResults = props.list.filter(element =>
+    element.type.includes(props.searchInput)
   );
+
+  if (!props.searchInput || searchResults.length > 0) {
+    return searchResults.map(element => (
+      <li key={element.id}>
+        {element.type} {element.calorie}
+      </li>
+    ));
+  } else {
+    return (
+      <div>
+        <h3>Item not found! You can manually add your item here:</h3>
+        <input id="manual-food" type="text" placeholder="Add food item here" />
+        <input
+          id="manual-calorie"
+          type="text"
+          placeholder="Add estimated calorie here"
+        />
+        <button>Add to Daily Log</button>
+      </div>
+    );
+  }
 }
 
 export default App;
